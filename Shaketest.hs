e4a85827a9fffd47db6d7b14c026c28b167ba304
Louis Pan <louis@pan.me>
2019-10-12 20:01:10 +1100

    simplified example

--- Shaketest.hs
+++ Shaketest.hs
@@ -5,12 +5,9 @@ main :: IO ()
 main = shakeArgs shakeOptions $ do
     want ["foo/.bar"]
 
-    "foo/.hello" %> \out -> do
-        cmd_ "touch foo/.hello"
-
     "foo/.bar" %> \out -> do
         cmd_ "mkdir -p foo"
         ls <- liftIO $ getDirectoryFilesIO "." ["*"]
         liftIO $ putStrLn $ show ls
-        void $ getDirectoryFiles "foo" ["*/.hello"]
+        void $ getDirectoryFiles "foo" ["*/.bar"]
         cmd_ "touch foo/.bar"
