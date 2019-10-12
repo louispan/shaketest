cd909662446c9c6227129fb5b740039ecb736026
Louis Pan <louis@pan.me>
2019-10-13 06:23:46 +1100

    don't use getDirectoryFiles for generated files https://github.com/ndmitchell/shake/issues/729#issuecomment-541336356

--- Shaketest.hs
+++ Shaketest.hs
@@ -1,13 +1,27 @@
 import Control.Monad
 import Development.Shake
+import System.Exit
 
 main :: IO ()
 main = shakeArgs shakeOptions $ do
-    want ["foo/.bar"]
+    want ["foo/.done"]
 
-    "foo/.bar" %> \out -> do
-        cmd_ "mkdir -p foo"
-        ls <- liftIO $ getDirectoryFilesIO "." ["*"]
-        liftIO $ putStrLn $ show ls
-        void $ getDirectoryFiles "foo" ["*/.bar"]
-        cmd_ "touch foo/.bar"
+    -- run work with same key to avoid re-running for each need
+    work <- newCache $ \_ -> do
+        liftIO $ putStrLn "touching"
+        cmd_ "mkdir -p foo/1"
+        cmd_ "mkdir -p foo/2"
+        cmd_ "touch foo/1/.bar"
+        cmd_ "touch foo/2/.bar"
+
+    "foo/*/.bar" %> \out -> work "once only"
+
+    "foo/.once" %> \out -> do
+        work "once only"
+        cmd_ "touch foo/.once"
+
+    "foo/.done" %> \out -> do
+        need ["foo/.once"]
+        Stdout ls <- cmd "find foo -name .bar"
+        need $ words ls
+        cmd_ "touch foo/.done"
